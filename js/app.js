fb74030894abbfe3853941f34426c16f663ab7e5
Daniel <duquedk1@gmail.com>
2021-08-28 11:51:16 -0500

    update app.js finished function guess

--- js/app.js
+++ js/app.js
@@ -1,33 +1,28 @@
-let correct=0;
+let correct=0
+let score =0
 
 game()
 
-$('.option').on('click', function(){
-    let index = $('.option').index(this)
-    if(index === correct){
-        alert('Muy bien!!!!!!!!!!!!!!')
-    }else{
-        alert('Intentalo de nuevo')
-    }
-    game()
-})
+$('.option').on('click', guess)
 
-/*function guess(){
+function guess(){
     let index = $('.option').index(this)
     if(index === correct){
         alert('Muy bien!!!!!!!!!!!!!!')
+        score++
     }else{
         alert('Intentalo de nuevo')
+        score = 0
     }
+    $('.score span').text(score)
     game()
-}*/
+}
 
 function game(){
     correct = Math.floor(Math.random()*2);
     $('.option').each(function(index) {
         let color = generateColor();
         $(this).css('background-color', color) // como this nos deja un color aleatorio para cada circulo
-        
         if (index === correct) {
             $('.question').text(color)
         }
